a508252315bae53e8acd174be4d9f020e88d6386
John Garrett <infantrygarrett@icloud.com>
2019-07-31 21:03:07 -0500

    added context for contacts called ContactState

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
 const Navbar = ({ title, icon }) => {
   return (
@@ -9,12 +9,12 @@ const Navbar = ({ title, icon }) => {
         <i className={icon} /> {title}
       </h1>
       <ul>
-          <li>
-              <Link to='/'>Home</Link>
-          </li>
-          <li>
-              <Link to='/about'>About</Link>
-          </li>
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
+        <li>
+          <Link to='/about'>About</Link>
+        </li>
       </ul>
     </div>
   );
