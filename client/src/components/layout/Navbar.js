37fedfab26c161d7b82f91a82dcc1f856dc940c0
John Garrett <infantrygarrett@icloud.com>
2019-08-29 10:53:20 -0500

    added guest links

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,7 +1,7 @@
-import React, { Fragment, useContext } from "react";
-import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
-import AuthContext from "../../context/auth/authContext";
+import React, { Fragment, useContext } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import AuthContext from '../../context/auth/authContext';
 
 const Navbar = ({ title, icon }) => {
   const authContext = useContext(AuthContext);
@@ -11,30 +11,42 @@ const Navbar = ({ title, icon }) => {
     <Fragment>
       <li>Hello {user && user.name}</li>
       <li>
-        <a href="#!">
-          <i className="fas fa-sign-out-alt"></i>
-          <span className="hide-sm">Logout</span>
+        <a href='#!'>
+          <i className='fas fa-sign-out-alt' />
+          <span className='hide-sm'>Logout</span>
+        </a>
+      </li>
+    </Fragment>
+  );
+
+  const guestLinks = (
+    <Fragment>
+      <li>Hello {user && user.name}</li>
+      <li>
+        <a href='#!'>
+          <i className='fas fa-sign-out-alt' />
+          <span className='hide-sm'>Logout</span>
         </a>
       </li>
     </Fragment>
   );
   return (
-    <div className="navbar bg-primary">
+    <div className='navbar bg-primary'>
       <h1>
         <i className={icon} /> {title}
       </h1>
       <ul>
         <li>
-          <Link to="/">Home</Link>
+          <Link to='/'>Home</Link>
         </li>
         <li>
-          <Link to="/about">About</Link>
+          <Link to='/about'>About</Link>
         </li>
         <li>
-          <Link to="/register">Register</Link>
+          <Link to='/register'>Register</Link>
         </li>
         <li>
-          <Link to="/login">Login</Link>
+          <Link to='/login'>Login</Link>
         </li>
       </ul>
     </div>
@@ -43,11 +55,11 @@ const Navbar = ({ title, icon }) => {
 
 Navbar.propTypes = {
   title: PropTypes.string.isRequired,
-  icon: PropTypes.string
+  icon: PropTypes.string,
 };
 
 Navbar.defaultProps = {
-  title: "Contact App",
-  icon: "fas fa-id-card-alt"
+  title: 'Contact App',
+  icon: 'fas fa-id-card-alt',
 };
 export default Navbar;
