ab27a7e646d1ab9f2997c46514a2c3706dc6b462
John Garrett <infantrygarrett@icloud.com>
2019-08-29 11:12:27 -0500

    home page is a private route

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,17 @@
-import React, { Fragment } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Navbar from "./components/layout/Navbar";
-import Home from "./components/pages/Home";
-import About from "./components/pages/About";
-import ContactState from "./context/contact/ContactState";
-import AuthState from "./context/auth/AuthState";
-import Register from "./components/auth/Register";
-import Login from "./components/auth/Login";
-import AlertState from "./context/alert/AlertState";
-import Alerts from "./components/layout/Alerts";
-import setAuthToken from "./utils/setAuthToken";
-import "./App.css";
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+import ContactState from './context/contact/ContactState';
+import PrivateRoute from './components/routing/PrivateRoute';
+import AuthState from './context/auth/AuthState';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+import AlertState from './context/alert/AlertState';
+import Alerts from './components/layout/Alerts';
+import setAuthToken from './utils/setAuthToken';
+import './App.css';
 
 if (localStorage.token) {
   setAuthToken(localStorage.token);
@@ -23,14 +24,14 @@ const App = () => {
           <Router>
             <Fragment>
               <Navbar />
-              <div className="container">
+              <div className='container'>
                 <Alerts />
                 <Switch>
-                  <Route exact path="/" component={Home} />
-                  <Route exact path="/login" component={Login} />
+                  <PrivateRoute exact path='/' component={Home} />
+                  <Route exact path='/login' component={Login} />
 
-                  <Route exact path="/about" component={About} />
-                  <Route exact path="/register" component={Register} />
+                  <Route exact path='/about' component={About} />
+                  <Route exact path='/register' component={Register} />
                 </Switch>
               </div>
             </Fragment>
