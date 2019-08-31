9b2998ad00f8e385ec92b7efea333030c2ff98aa
John Garrett <infantrygarrett@icloud.com>
2019-08-31 13:52:40 -0500

    heroku issues

---
+++ client/build/precache-manifest.d19bbd91a5a8c0426ae12f92c786c0e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "080d878895e7364644a8c6055ddbc461",
+    "url": "/index.html"
+  },
+  {
+    "revision": "57d721185aeb3d27d9bf",
+    "url": "/static/css/main.01326b7f.chunk.css"
+  },
+  {
+    "revision": "691b0f9000e3704c40f6",
+    "url": "/static/js/2.5e20c240.chunk.js"
+  },
+  {
+    "revision": "57d721185aeb3d27d9bf",
+    "url": "/static/js/main.65f56b6c.chunk.js"
+  },
+  {
+    "revision": "83e7f8b54e61b1cbddf8",
+    "url": "/static/js/runtime~main.e38743dc.js"
+  }
+]);
--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const connectDB = require('./config/db');
-const path = require('path');
 
 const app = express();
 
@@ -15,19 +14,6 @@ app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
 
-// Serve static assets in production
-if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('client/build'));
+const PORT = process.env.PORT || 5000;
 
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  );
-}
-
-const port = process.env.PORT || 8000;
-
-
-server.listen(port, () => {
-  console.log('App is running on port ' + port);
-});
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
