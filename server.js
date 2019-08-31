33d1494e4ee8069c85638a8f785b006425f0f01f
John Garrett <infantrygarrett@icloud.com>
2019-08-31 00:11:26 -0500

    port issue

--- server.js
+++ server.js
@@ -25,9 +25,8 @@ if (process.env.NODE_ENV === 'production') {
   );
 }
 
-// const PORT = process.env.PORT || 5000;
 
-app.listen(process.env.PORT || 3000, function() {
+app.listen(process.env.PORT || 3000, () => {
   console.log(
     'Express server listening on port %d in %s mode',
     this.address().port,
